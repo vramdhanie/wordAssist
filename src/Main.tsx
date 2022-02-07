0074d4942859faf26f3c5be57876752a4a6f5548
Vincent Ramdhanie <vramdhanie@gmail.com>
2022-02-07 09:12:05 -0400

    Add letter position

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,6 @@
 import { styled } from './config/stitches.config'
 import { words } from './data/words'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useCallback} from 'react'
 
 const Wrapper = styled('div', {
     flex: '1',
@@ -61,12 +61,40 @@ const Information = styled('div', {
 
 const Input = styled('input', {})
 
+const Button = styled('button', {
+
+})
+
+const FixedLetters = styled('div', {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: '4px'
+})
+
+const FixedInput = styled('input', {
+    width: '150px',
+    height: '150px',
+    fontSize: '60px',
+    fontWeight: 'bold',
+    textAlign: 'center'
+})
+
+
+
 export default function () {
     const [lettersToInclude, setLetterstoInclude] = useState<string>('')
     const [lettersToExclude, setLetterstoExclude] = useState<string>('')
     const [wordsToDisplay, setWordsToDisplay] = useState<string[]>(words)
     const [showing, setShowing] = useState('')
     const [hiding, setHiding] = useState('')
+    const [fixed, setFixed] = useState(['', '', '', '', ''])
+
+    const handleReset = useCallback(() => {
+        setLetterstoExclude('')
+        setLetterstoInclude('')
+        setFixed(['', '', '', '', ''])
+    }, [showing, hiding])
 
     useEffect(() => {
         if(lettersToInclude.trim() === '') {
@@ -88,17 +116,25 @@ export default function () {
     }, [lettersToExclude])
 
     useEffect(() => {
-        if(showing.length === 0 && hiding.length === 0) {
+        if(fixed.join('').length === 0 && showing.length === 0 && hiding.length === 0) {
             setWordsToDisplay(words)
             return
         }
         const show = showing.split('')
         const hide = hiding.split('')
-        setWordsToDisplay(words.filter(word => {
+        let wordsWithLetters = words.filter(word => {
             const w = word.split('')
             return show.every(c => w.includes(c)) && hide.every(c => !w.includes(c))
-        }))
-    }, [showing, hiding])
+        })
+        if(fixed.join('').length > 0) {
+           fixed.forEach((c, i) => {
+               if(c !== '') {
+                   wordsWithLetters = wordsWithLetters.filter(word => word[i] === c)
+               }
+           })
+        }
+        setWordsToDisplay(wordsWithLetters)
+    }, [showing, hiding, fixed])
 
     return <Wrapper>
         <Form>
@@ -107,10 +143,35 @@ export default function () {
                 <Input onChange={e => setLetterstoInclude(e.target.value)} value={lettersToInclude}/>
             </Box>
             <Box>
+                <Button onClick={handleReset}>Reset</Button>
+            </Box>
+            <Box>
                 <Label>Letters to Exclude</Label>
                 <Input onChange={e=>setLetterstoExclude(e.target.value)} value={lettersToExclude}/>
             </Box>
         </Form>
+        <FixedLetters>
+            <FixedInput onChange={e => setFixed(f => {
+                f[0] = e.target.value.length > 0 ? e.target.value[0] : ''
+                return [...f]
+            })} value={fixed[0]}/>
+            <FixedInput onChange={e => setFixed(f => {
+                f[1] = e.target.value.length > 0 ? e.target.value[0] : ''
+                return [...f]
+            })} value={fixed[1]}/>
+            <FixedInput onChange={e => setFixed(f => {
+                f[2] = e.target.value.length > 0 ? e.target.value[0] : ''
+                return [...f]
+            })} value={fixed[2]}/>
+            <FixedInput onChange={e => setFixed(f => {
+                f[3] = e.target.value.length > 0 ? e.target.value[0] : ''
+                return [...f]
+            })} value={fixed[3]}/>
+            <FixedInput onChange={e => setFixed(f => {
+                f[4] = e.target.value.length > 0 ? e.target.value[0] : ''
+                return [...f]
+            })} value={fixed[4]}/>
+        </FixedLetters>
         <Information>
             <Box>{showing || hiding ? <Box>{showing && <Text>Showing words containing the letters: {showing}</Text>}
                     {hiding && <Text>Hiding words containing the letters: {hiding}</Text>}
